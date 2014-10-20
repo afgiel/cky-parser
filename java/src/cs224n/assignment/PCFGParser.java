3a02732f68e6b3b2dd21a94621248223d0cf2f98
Andrew Francis Giel <agiel@stanford.edu>
2014-10-20 00:22:58 -0700

    added PCFGParser

--- java/src/cs224n/assignment/PCFGParser.java
+++ java/src/cs224n/assignment/PCFGParser.java
@@ -1,6 +1,7 @@
 package cs224n.assignment;
 
 import cs224n.ling.Tree;
+import cs224n.util.CounterMap;
 import java.util.*;
 
 /**
@@ -11,14 +12,17 @@ public class PCFGParser implements Parser {
     private Lexicon lexicon;
 
     public void train(List<Tree<String>> trainTrees) {
-        // TODO: before you generate your grammar, the training trees
-        // need to be binarized so that rules are at most binary
-        lexicon = new Lexicon(trainTrees);
-        grammar = new Grammar(trainTrees);
+        List<Tree<String>> annotatedTrainTrees = new ArrayList<Tree<String>>(); 
+        for (Tree trainTree : trainTrees) {
+          annotatedTrainTrees.add(TreeAnnotations.annotateTree(trainTree));    
+        }
+        lexicon = new Lexicon(annotatedTrainTrees);
+        grammar = new Grammar(annotatedTrainTrees);
     }
 
     public Tree<String> getBestParse(List<String> sentence) {
-        // TODO: implement this method
+        CounterMap<String, String> grid = new CounterMap<String, String>(); 
+        // Pair(x, y).toString then symbol to float  
         return null;
     }
 }
